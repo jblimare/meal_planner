39851d0f342a2ce6f99af4ecf41168c8ffa317cf
jblimare <jblimare@gmail.com>
2018-08-09 21:48:43 +0100

    Creation of Recipe model + added to the admin panel

--- meal_planners/models.py
+++ meal_planners/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    """A recipe that the user can create and view"""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(default="Please enter description of your recipe")
+
+    def __str__(self):
+        """Return a string representationof the model"""
+        return self.name
+
