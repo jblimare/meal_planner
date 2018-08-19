b1234bcad484984c509376e5692547f34b51bb2d
jblimare <jblimare@gmail.com>
2018-08-19 21:45:02 +0100

    View recipe description

--- meal_planners/views.py
+++ meal_planners/views.py
@@ -16,5 +16,6 @@ def recipes(request):
 def recipe(request, recipe_id):
     """Show a single recipe and its description"""
     recipe = Recipe.objects.get(id=recipe_id)
-    context = {'recipe': recipe}
+    description = recipe.description
+    context = {'recipe': recipe, 'description': description}
     return render(request, 'meal_planners/recipe.html', context)
