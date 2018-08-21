fb3e616be0933cfb5cc984bebebe6aab1cbf455b
jblimare <jblimare@gmail.com>
2018-08-21 09:16:12 +0100

    Heroku debug true

--- meal_planner/settings.py
+++ meal_planner/settings.py
@@ -141,7 +141,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     
     # Only allow heroku to host the project.
     ALLOWED_HOSTS = ['*']
-    DEBUG = False
+    DEBUG = True
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
